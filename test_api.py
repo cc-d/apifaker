072dad42451ab52e69db31bdf44b06fe2e41b252
Cary Carter <ccarterdev@gmail.com>
2024-04-11 18:39:24 -0700

    test data

--- test_api.py
+++ test_api.py
@@ -2,67 +2,52 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.testclient import TestClient
 from pydantic import BaseModel
-from pyshared import ran, List, Opt
+from pyshared import ran, List, Opt, RanData
 
 app = FastAPI()
 
+rd = RanData()
 
-class IDBase(BaseModel):
+
+class Prop(BaseModel):
+    pstr: str = rd.str
+    pbool: bool = rd.bool
+    pint: int = rd.int
+    pfloat: float = rd.float
+
+
+class Item(BaseModel):
     id: int
+    parent: Opt["Item"] = None
+    children: List["Item"] = []
+    props: List[Prop] = [Prop() for _ in range(0, 2)]
 
 
-class Person(IDBase):
-    name: str
-    age: int
+def gen_items(n=10) -> List[Item]:
+    pool = [x for x in range(1, n)]
+    parents = [pool.pop() for _ in range(n // 5)]
+    parents = [Item(id=x) for x in parents]
+    items = parents.copy()
+    while pool:
+        parent = ran.choice(parents)
+        child = pool.pop()
+        items.append(Item(id=child, parent=parent))
+    return items
 
 
-class Building(IDBase):
-    address: str
-    owner: Person
+items = gen_items()
 
 
-class House(Building):
-    occupants: List[Person]
+class Group(BaseModel):
+    id: int
+    items: List[Item] = []
+    props: List[Prop] = [Prop() for _ in range(0, 2)]
 
 
-class Data:
-    people: List[Person] = []
-    houses: List[House] = []
-    homeless: List[Person] = []
-    housed: List[Person] = []
+groups = [Group(id=i, items=items) for i in [1, 2]]
 
-    def __init__(self):
-        for i in range(100):
-            self.people.append(
-                Person(id=i, name=f'Person {i}', age=ran.randint(1, 100))
-            )
-        pool = self.people.copy()
-        for i in range(10):
-            occupants = [
-                pool.pop(ran.randint(0, len(pool) - 1))
-                for _ in range(ran.randint(1, 7))
-            ]
-            self.houses.append(
-                House(
-                    id=i,
-                    address=f'Address {i}',
-                    owner=pool.pop(ran.randint(0, len(pool) - 1)),
-                    occupants=occupants,
-                )
-            )
-        self.homeless = pool
-        self.housed = [p for h in self.houses for p in h.occupants]
-
-    def __repr__(self):
-        return "Data(people={}, houses={}, homeless={}, housed={})".format(
-            len(self.people),
-            len(self.houses),
-            len(self.homeless),
-            len(self.housed),
-        )
-
-
-data = Data()
+for i in items:
+    groups[0 if i.id % 2 == 0 else 1].items.append(i)
 
 
 @app.get("/openapi.json")
@@ -70,11 +55,17 @@ async def openapi():
     return app.openapi()
 
 
-@app.get("/people")
-async def get_people():
-    return data.people
+@app.get("/items", response_model=List[Item])
+async def get_items():
+    return items
+
+
+@app.get("/groups", response_model=List[Group])
+async def get_groups():
+    return groups
 
 
 client = TestClient(app)
 
-print(str(client.get("/people").json())[0:100], data, sep="\n")
+# print(str(client.get("/openapi.json").json())[0:1000])
+print(str(client.get("/items").json())[0:1000])
